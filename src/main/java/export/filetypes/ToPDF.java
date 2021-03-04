84e035e5f9d0f4d73a69e8f81cd4c1c6979131d7
Niek Flipse <npaflips@avans.nl>
2021-03-04 10:46:24 +0100

    added sout to export methods

--- src/main/java/export/filetypes/ToPDF.java
+++ src/main/java/export/filetypes/ToPDF.java
@@ -5,6 +5,8 @@ import export.Report;
 public class ToPDF implements IExportToFile {
     @Override
     public void export(Report report) {
-
+        System.out.println("----------{PDF}----------");
+        System.out.println(report.toString());
+        System.out.println("----------{PDF}----------");
     }
 }
--- src/main/java/export/filetypes/ToPNG.java
+++ src/main/java/export/filetypes/ToPNG.java
@@ -5,6 +5,8 @@ import export.Report;
 public class ToPNG implements IExportToFile {
     @Override
     public void export(Report report) {
-
+        System.out.println("----------{PNG}----------");
+        System.out.println(report.toString());
+        System.out.println("----------{PNG}----------");
     }
 }
